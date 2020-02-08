c175a0790513757ba01c2e9ce20ffda74158a85f
gulnaraY <guga.ufa@gmail.com>
2020-02-08 12:03:18 +0300

    снепшот-тесты

---
+++ src/components/welcome-screen/welcome-screen.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import rerender from 'react-test-renderer';
+import WelcemeScreen from './welcome-screen.jsx';
+
+const errorsCount = 6;
+
+it(`Should render WelcomeScreen`, () => {
+  const tree = rerender
+    .create(
+        <WelcemeScreen
+          errorsCount = {errorsCount}
+        />)
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
